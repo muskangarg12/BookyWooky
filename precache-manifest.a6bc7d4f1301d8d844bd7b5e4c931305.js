630c6acec787e3363710d3ab9850f6f7bcfa678d
muskangarg12 <muskangargnsit@gmail.com>
2019-03-22 00:44:50 +0530

    Updates

---
+++ precache-manifest.a6bc7d4f1301d8d844bd7b5e4c931305.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "01b4be69fda30e9a0feb",
+    "url": "/BookyWooky/static/js/runtime~main.f227ea1a.js"
+  },
+  {
+    "revision": "d4b8fd8d57ba5bbe053a",
+    "url": "/BookyWooky/static/js/main.88a11882.chunk.js"
+  },
+  {
+    "revision": "84d788e5adc7d46bba2a",
+    "url": "/BookyWooky/static/js/2.6a0a6e1f.chunk.js"
+  },
+  {
+    "revision": "d4b8fd8d57ba5bbe053a",
+    "url": "/BookyWooky/static/css/main.2262a6e5.chunk.css"
+  },
+  {
+    "revision": "84d788e5adc7d46bba2a",
+    "url": "/BookyWooky/static/css/2.ac3cbb0d.chunk.css"
+  },
+  {
+    "revision": "26a3e7f942a52c0d6fb124401aeab6f1",
+    "url": "/BookyWooky/index.html"
+  }
+];
